03574abd520b45c73795919c84f1a2e08f309846
Ralf S. Engelschall <rse@engelschall.com>
2017-07-02 11:44:14 +0200

    initial backend API

--- src/gemstone.js
+++ src/gemstone.js
@@ -8,15 +8,16 @@
  *  The external dependencies
  */
 
-/*  FIXME  */
+const Microkernel = require("microkernel")
+const SysPath     = require("syspath")
+const Latching    = require("latching")
 
 /*
  *  The Gemstone API
  */
 
-import Latching from "latching"
-
 let latching = new Latching()
+let kernel   = null
 
 export default class Gemstone {
     /*  latching integration  */
@@ -30,25 +31,74 @@ export default class Gemstone {
 
         /*  default options  */
         options = Object.assign({
-            app:    "example",
-            config: { env: "development", tag: "" },
-            ui:     () => [ "root", {}, "visible" ],
-            sv:     (url, cid) => {}
+            app:     "example",
+            ini:     "example.ini",
+            modules: "**/*.mod.js",
+            config:  { env: "development", tag: "" }
         }, options)
         latching.hook("boot-options", "pass", options)
 
-        /*  FIXME  */
+        /*  instanciate microkernel  */
+        kernel = new Microkernel()
 
-        latching.hook("boot-leave", "none")
+        /*  define state transitions  */
+        kernel.transitions([
+            { state: "dead",       enter: null,        leave: null },
+            { state: "booted",     enter: "boot",      leave: "shutdown" },
+            { state: "latched",    enter: "latch",     leave: "unlatch" },
+            { state: "configured", enter: "configure", leave: "reset" },
+            { state: "prepared",   enter: "prepare",   leave: "release" },
+            { state: "started",    enter: "start",     leave: "stop" }
+        ])
+
+        /*  define module groups  */
+        kernel.groups([ "BOOT", "BASE", "RESOURCE", "SERVICE", "USECASE" ])
+
+        /*  execute extensions on microkernel  */
+        await kernel.exec("microkernel-ext-co")
+        if (process.env.DEBUG)
+            await kernel.exec("microkernel-ext-debug")
+
+        /*  determine system paths  */
+        const syspath = SysPath({ appName: options.app })
+        kernel.rs("syspath", syspath)
+
+        /*  load modules into microkernel  */
+        kernel.load(
+            "microkernel-mod-ctx",
+            [ "microkernel-mod-options", { inifile: options.ini } ],
+            "microkernel-mod-logger",
+            "microkernel-mod-daemon",
+            "microkernel-mod-title",
+            "microkernel-mod-shutdown",
+            options.modules
+        )
+
+        /*  startup microkernel and its modules  */
+        return kernel.state("started").then(() => {
+            kernel.publish("app:start:success")
+            latching.hook("boot-leave", "none", null)
+        }).catch((err) => {
+            kernel.publish("app:start:error", err)
+            latching.hook("boot-leave", "none", err)
+            throw err
+        })
     }
 
     /*  the shutdown API method  */
     static async shutdown () {
+        /*  shutdown microkernel and its modules  */
         latching.hook("shutdown-enter", "none")
-
-        /*  FIXME  */
-
-        latching.hook("shutdown-leave", "none")
+        return kernel.state("booted").then(() => {
+            kernel.publish("app:start:success")
+            latching.hook("shutdown-leave", "none", null)
+            kernel = null
+        }).catch((err) => {
+            kernel.publish("app:start:error", err)
+            latching.hook("shutdown-leave", "none", err)
+            kernel = null
+            throw err
+        })
     }
 }
 
